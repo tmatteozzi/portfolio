b2f7bc009f95c8f189b3fe7197f94b2c7f29a6dd
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-05-19 15:10:21 -0300

    First Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import About from './pages/About';
+import Layout from './pages/Layout';
 
 export default function Home() {
     return (
         <div>
+            <Layout />
             <About />
         </div>
     );
---
+++ app/pages/Layout.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { motion } from 'framer-motion';
+import { HeroHighlight, Highlight } from '../components/ui/hero-highlight';
+
+export default function Layout() {
+    return (
+        <div>
+            <HeroHighlight>
+                <motion.h1
+                    initial={{
+                        opacity: 0,
+                        y: 20
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: [20, -5, 0]
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: [0.4, 0.0, 0.2, 1]
+                    }}
+                    className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+                >
+                    Hi! I am{' '}
+                    <Highlight className="text-black dark:text-white">
+                        Tomás Agustín Matteozzi
+                    </Highlight>
+                    !
+                </motion.h1>
+                <motion.p
+                    initial={{
+                        opacity: 0,
+                        y: 20
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: [20, -5, 0]
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: [0.4, 0.0, 0.2, 1]
+                    }}
+                    className="text-l px-4 md:text-l lg:text-xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+                >
+                    A passionate software developer committed to creating
+                    efficient and innovative solutions. Constantly learning and
+                    evolving, I bring a diverse set of skills to each project,
+                    enabling me to deliver high-quality, scalable applications.
+                </motion.p>
+            </HeroHighlight>
+        </div>
+    );
+}
