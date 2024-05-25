13a5bf8df2db65a1484e5b21daf7035b9ea88d9b
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-05-25 16:55:36 -0300

    Footer changes

--- app/pages/Footer.tsx
+++ app/pages/Footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
     return (
         <footer className="bg-gray-50 text-center py-8">
             <div className="container mx-auto">
-                <h2 className="text-l md:text-xl lg:text-2xl font-bold text-neutral-700">
+                <h2 className="text-md md:text-l lg:text-xl font-bold text-neutral-700">
                     © Tomás Agustín Matteozzi
                 </h2>
-                <p className="text-md md:text-lg lg:text-xl text-neutral-600">
+                <p className="text-sm md:text-md lg:text-l font-semibold text-neutral-600">
                     Software Engineer
                 </p>
                 <div className="flex justify-center space-x-6 mt-2">
@@ -17,7 +17,7 @@ export default function Footer() {
                         rel="noopener noreferrer"
                         className="text-gray-700 dark:text-white hover:text-indigo-300 transition-colors duration-300"
                     >
-                        <FaGithub size={30} />
+                        <FaGithub size={25} />
                     </a>
                     <a
                         href="https://www.linkedin.com/in/tomás-matteozzi-452b3728b"
@@ -25,7 +25,7 @@ export default function Footer() {
                         rel="noopener noreferrer"
                         className="text-gray-700 dark:text-white hover:text-indigo-300 transition-colors duration-300"
                     >
-                        <FaLinkedin size={30} />
+                        <FaLinkedin size={25} />
                     </a>
                 </div>
             </div>
