6a9566c5524ade4602b080b922f9ab48e659897d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-05-25 20:40:59 -0300

    qchanges

--- app/components/ui/hero-parallax.tsx
+++ app/components/ui/hero-parallax.tsx
@@ -150,8 +150,8 @@ export const ProductCard = ({
                     alt={product.title}
                 />
             </Link>
-            <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
-            <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
+            <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-gradient-to-r from-indigo-50 to-purple-50 pointer-events-none"></div>
+            <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-black font-semibold">
                 {product.title}
             </h2>
         </motion.div>
