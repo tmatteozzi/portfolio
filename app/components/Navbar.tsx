dbc51803449102ec287faec576636d677dc73d13
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-05-26 19:04:36 -0300

    Responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -89,7 +89,7 @@ export default function Navbar() {
                 </div>
                 <button onClick={downloadPdf} className="p-[3px] relative mr-4">
                     <div className="absolute inset-0 bg-gradient-to-r from-indigo-300 to-purple-300 rounded-lg" />
-                    <div className="px-8 py-2 bg-white rounded-[6px] relative group transition duration-200 text-black font-semibold hover:bg-transparent dark:text-white dark:bg-black dark:hover:text-black">
+                    <div className="px-8 py-2 bg-white rounded-[6px] relative group transition duration-200 text-black font-semibold hover:bg-transparent dark:text-white dark:bg-black dark:hover:text-black text-xs md:text-base">
                         Resume
                     </div>
                 </button>
