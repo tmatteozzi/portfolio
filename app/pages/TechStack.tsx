85987513763a95097d1c76d95215d8d8ec0cac0a
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-06-19 20:52:21 -0300

    TechStack reusable

--- app/pages/TechStack.tsx
+++ app/pages/TechStack.tsx
@@ -1,69 +1,13 @@
 import React from 'react';
-import HtmlIcon from '../assets/icons/html.svg';
-import JavaIcon from '../assets/icons/java.svg';
-import JavascriptIcon from '../assets/icons/javascript.svg';
-import TypescriptIcon from '../assets/icons/typescript.svg';
-import PythonIcon from '../assets/icons/python.svg';
-import CssIcon from '../assets/icons/css.svg';
-import ReactIcon from '../assets/icons/react.svg';
-import MySQLIcon from '../assets/icons/mysql.svg';
-import TailwindIcon from '../assets/icons/tailwind.svg';
-import NextJSIcon from '../assets/icons/nextjs.svg';
-import ViteIcon from '../assets/icons/vite.svg';
-import LinuxIcon from '../assets/icons/linux.svg';
 
-export default function TechStack() {
-    const techStackItems = [
-        {
-            name: 'Python',
-            img: PythonIcon
-        },
-        {
-            name: 'Java',
-            img: JavaIcon
-        },
-        {
-            name: 'Javascript',
-            img: JavascriptIcon
-        },
-        {
-            name: 'Typescript',
-            img: TypescriptIcon
-        },
-        {
-            name: 'React',
-            img: ReactIcon
-        },
-        {
-            name: 'NextJS',
-            img: NextJSIcon
-        },
-        {
-            name: 'Vite',
-            img: ViteIcon
-        },
-        {
-            name: 'HTML',
-            img: HtmlIcon
-        },
-        {
-            name: 'CSS',
-            img: CssIcon
-        },
-        {
-            name: 'MySQL',
-            img: MySQLIcon
-        },
-        {
-            name: 'Linux',
-            img: LinuxIcon
-        },
-        {
-            name: 'Tailwind',
-            img: TailwindIcon
-        }
-    ];
+interface TechStackProps {
+    techStack: {
+        name: string;
+        img: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    }[];
+}
 
+export default function TechStack({ techStack }: TechStackProps) {
     return (
         <section
             id="tech-stack"
@@ -73,7 +17,7 @@ export default function TechStack() {
                 Tech Stack
             </h1>
             <div className="flex flex-col sm:flex-row items-center justify-center flex-wrap gap-8 max-w-6xl mx-auto">
-                {techStackItems.map((item, index) => (
+                {techStack.map((item, index) => (
                     <div
                         key={index}
                         className="w-44 h-40 rounded-3xl p-4 shadow-xl border dark:border-gray-800 flex justify-center items-center transform transition-transform duration-300 hover:scale-105 dark:bg-gray-900"
