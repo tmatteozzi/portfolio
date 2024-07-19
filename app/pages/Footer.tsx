bbae937d09e930e63f3338b6580b1ae1720642a4
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-07-19 19:43:12 -0300

    Footer

--- app/pages/Footer.tsx
+++ app/pages/Footer.tsx
@@ -12,12 +12,12 @@ export default function Footer({ content }: FooterProps) {
     const { name, role, socialLinks } = content;
 
     return (
-        <footer className="bg-white dark:bg-gray-950 text-center py-6">
+        <footer className="bg-gray-950 text-center py-6">
             <div className="container mx-auto">
-                <h2 className="text-md md:text-l lg:text-xl font-bold text-neutral-700 dark:text-white">
+                <h2 className="text-md md:text-l lg:text-xl font-bold text-white">
                     © {name}
                 </h2>
-                <p className="text-sm md:text-md lg:text-l font-semibold text-neutral-600 dark:text-neutral-400">
+                <p className="text-sm md:text-md lg:text-l font-semibold text-neutral-400">
                     {role}
                 </p>
                 <div className="flex justify-center space-x-6 mt-2">
@@ -27,7 +27,7 @@ export default function Footer({ content }: FooterProps) {
                             href={link.url}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="text-gray-700 dark:text-white hover:text-indigo-300 dark:hover:text-indigo-500 transition-colors duration-300"
+                            className="text-white hover:text-indigo-500 transition-colors duration-300"
                         >
                             {link.icon}
                         </a>
