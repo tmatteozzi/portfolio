61366b740819068b1d5468db35683c95115fa21d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-09-01 18:40:38 -0300

    TechStack Changes

--- app/pages/TechStack.tsx
+++ app/pages/TechStack.tsx
@@ -20,11 +20,11 @@ export default function TechStack({ techStack }: TechStackProps) {
       <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-white leading-relaxed lg:leading-snug mb-16 mx-auto">
         Tech Stack
       </h1>
-      <div className="flex flex-col sm:flex-row items-center justify-center flex-wrap gap-8 max-w-6xl mx-auto">
+      <div className="flex sm:flex-row items-center justify-center flex-wrap gap-8 max-w-6xl mx-auto">
         {sortedTechStack.map((item, index) => (
           <div
             key={index}
-            className="w-44 h-40 rounded-3xl p-4 shadow-xl border border-gray-300 hover:border-gray-100 flex justify-center items-center transform transition-transform duration-300 hover:scale-105"
+            className="w-40 h-36 sm:w-44 sm:h-40 rounded-3xl p-4 shadow-xl border border-gray-300 hover:border-gray-100 flex justify-center items-center transform transition-transform duration-300 hover:scale-105"
           >
             <div className="flex flex-col items-center p-4">
               <item.img width={80} height={80} />
